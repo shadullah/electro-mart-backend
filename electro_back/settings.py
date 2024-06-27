9a56ca856a9cd8a8366224df6cf094c5a73ba8a7
hexaalif <shadullahsad9@gmail.com>
2024-06-27 20:33:27 +0600

    error solved

--- electro_back/settings.py
+++ electro_back/settings.py
@@ -150,7 +150,7 @@ WSGI_APPLICATION = 'electro_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASS"),
