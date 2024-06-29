4548602303a72b1f640731ad2ad5e27d02053dfe
hexaalif <shadullahsad9@gmail.com>
2024-06-29 20:37:30 +0600

    category added

--- categories/models.py
+++ categories/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Category(models.Model):
     name=models.CharField(max_length=100)
     slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
--- seller/models.py
+++ seller/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from categories.models import Category
 # Create your models here.
 
 
@@ -9,7 +10,7 @@ class Item(models.Model):
     description= models.TextField(max_length=1200)
     price = models.IntegerField()
     condition=models.CharField(max_length=100)
-    # category = models
+    category = models.ForeignKey(Category,on_delete=models.CASCADE, null=True,default="Watch", blank=True)
     image = models.URLField(null=True, blank=True)
 
     def __str__(self) -> str:
